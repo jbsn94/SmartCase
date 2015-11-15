09234d1b0fc7c320474649aa9d81b8462b7b841d
Neto Barbosa <jbsn@cin.ufpe.br>
2015-11-15 19:37:06 +0000

    db update

--- db/migrate/20151115192427_users.rb
+++
@@ -1,5 +0,0 @@
-class Users < ActiveRecord::Migration
-  def change
-     drop_table :users
-  end
-end
---
+++ db/migrate/20151115193112_users.rb
@@ -0,0 +1,18 @@
+class Users < ActiveRecord::Migration
+  def change
+    create_table(:users) do |t|
+      t.column :name, :string
+      t.column :email, :string
+      t.column :cpf, :string
+      t.column :password, :string
+      t.column :tipo, :string
+    end
+    create_table(:machines) do |t|
+      t.column :tipping, :string
+      t.column :description, :text
+      t.column :model, :string
+      t.column :created_at, :datetime, null: false
+      t.column :updated_at, :datetime, null: false
+    end
+  end
+end
