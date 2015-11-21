4cf88d24a045e4d28ef3ec4e780bc6c3cb62e983
Neto Barbosa <jbsn@cin.ufpe.br>
2015-11-21 01:52:05 +0000

    Layout Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id] 
   end
   def require_user 
-    redirect_to '/', notice: 'Você não está logado' unless current_user 
+    redirect_to '/', notice: 'Você não está logado.' unless current_user 
   end
 end
