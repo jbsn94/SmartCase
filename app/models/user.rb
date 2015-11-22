0e890e0f4101914f0b5dd568b569948416af130a
victornun <vnfn@cin.ufpe.br>
2015-11-22 01:11:04 +0000

    Adicionado validações nos campos do model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+    validates :name, :email, :cpf, :password, :tipo, presence: true
+    validates :cpf , confirmation: true, uniqueness: { message: " already exists. Try again" }
+    validates :password, presence: true, length: { minimum: 10 , too_short: " must have %{count} characters"}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 100 },format: { with: VALID_EMAIL_REGEX }
 end
