2e65f6262b791ca1a77dac57352f523648400921
victornun <vnfn@cin.ufpe.br>
2015-11-22 04:26:39 +0000

    Adicionado testes  nos models user_test.rb e machine_test.rb

--- test/models/machine_test.rb
+++ test/models/machine_test.rb
@@ -4,4 +4,13 @@ class MachineTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "should not save if fields don't match with pattern" do
+    regex = /\A[a-zA-Z0-9]+\z/
+    @mach = Machine.new(tipping: "q9we18", description: "qDCQER8", model: "123_1981FFD")
+    assert_match regex, @mach.tipping
+    assert_match regex, @mach.description
+    assert_no_match regex, @mach.model
+    assert_not @mach.save
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,22 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "should save with all fields fill" do
+    @user = User.new(name: 'Neto', email: 'jbsn@cin.ufpe.br', cpf: '108843', password: '12345678910', tipo: 'Administrador')
+    assert @user.save
+  end
+
+  test "should not save without fill the user fields" do
+    @user = User.new(name: 'Neto', email: 'jbsn@cin.ufpe.br', cpf: '108843', password: '12345678910')
+    assert_not @user.save, "Saved the user without fill all fields"
+  end
+  
+  test "should have to validates email" do
+    regex = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+#    regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z0-9]+\z/i 
+    @user = User.new(email: 'jbsn@uf1pe.br')
+    assert_match regex, @user.email
+  end
+  
 end
