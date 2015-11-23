f33d92449a52b2722a888b16b8672513c8fb6456
Neto Barbosa <jbsn@cin.ufpe.br>
2015-11-23 05:57:29 +0000

    Layout Pagina

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_action :require_user
   
   def index
-    @users = User.count
+    @users = User.where(tipo: 'Solicitante').count
     @machines = Machine.count
     @funcionarios = User.where(tipo: 'Funcionário').count + User.where(tipo: 'Administrador').count
     @menuHome = "active-menu"
