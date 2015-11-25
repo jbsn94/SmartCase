a9e2d92dd1dd3f5f6fd38ee6d9e18db43e734dd1
Neto Barbosa <jbsn@cin.ufpe.br>
2015-11-25 21:28:25 +0000

    Atualização de erros BDD

--- features/remove_users.feature
+++ features/remove_users.feature
@@ -4,14 +4,6 @@ Feature: Remove users
   I want to remove users in the database
   
 Scenario:
-<<<<<<< HEAD
-  When I login the system
-  Given I am on the users home page
-  And I should see "User Name 2"
-  When I follow "Deletar usuário_2"
-  Then I should back to the users home page
-  And I should not see the user "User Name 2"
-=======
   Given I login the system
   And I have two or more Users
   When I am on the Users Page
@@ -20,4 +12,3 @@ Scenario:
   Then I should back to the users home page
     And I should see a text "User was successfully destroyed"
   And I should not see the user "User Name Teste"
->>>>>>> BDD
--- features/step_definitions/remove_user_step.rb
+++ features/step_definitions/remove_user_step.rb
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-Given(/^I am on the users home page$/) do
-  @user = User.new(name: "User Name 2", email: "username1@cin.ufpe", cpf: "123456", password: "123456", tipo: "Solicitante")
-  @user.save
-  visit(users_path)
-=======
 When(/^I have two or more Users$/) do
   @user2 = User.new(name: "User Name Teste", email: "username3@cin.ufpe", cpf: "123456", password: "123456", tipo: "Administrador")
   @user2.save
   page.has_content?(@user.name)
   page.has_content?(@user2.name)
->>>>>>> BDD
 end
 
 When(/^I follow delete user "(.*?)"$/) do |arg1|
@@ -21,11 +14,7 @@ Then(/^I should back to the users home page$/) do
 end
 
 Then(/^I should not see the user "(.*?)"$/) do |arg1|
-<<<<<<< HEAD
-    !page.has_content?(arg1)
-=======
     if page.has_content?(arg1)
       find( 'td', text: arg1, exact: true )
     end
->>>>>>> BDD
 end
