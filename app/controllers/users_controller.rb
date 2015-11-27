71e88fa14490c2d019c7120ff069c382f7b2aad6
Neto Barbosa <jbsn@cin.ufpe.br>
2015-11-27 14:52:21 +0000

    Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,6 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
-    @pass = (0...8).map { (97 + rand(25)).chr }.join
   end
 
   # GET /users/1/edit
@@ -34,6 +33,8 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+    @pass = (0...8).map { (97 + rand(25)).chr }.join
+    @user.password = @pass
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
