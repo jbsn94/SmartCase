409052bcbca745f665288eb071bc834986758909
Neto Barbosa <jbsn@cin.ufpe.br>
2015-12-01 14:42:00 +0000

    Atualizações de layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
     
     validates :name, :email, :cpf, :password, :tipo, presence: true
     validates :name, format: { with: VALID_NAME_REGEX }
-    validates :cpf, uniqueness: { message: " already exists. Try again" }, format: { with: VALID_CPF_REGEX }
+    validates :cpf, uniqueness: { message: "already exists. Try again" }, format: { with: VALID_CPF_REGEX }
 	validates :email, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(name: 'Neto', email: 'jbsn@cin.ufpe.br', cpf: '108843', password: '12345678910', tipo: 'Administrador')
+User.create(name: 'Solicitante Nome', email: 'solicitante@email.com', cpf: '123', password: '123', tipo: 'Solicitante')
+User.create(name: 'Funcionario Nome', email: 'funcionario@email.com', cpf: '1234', password: '1234', tipo: 'Funcionario')
+User.create(name: 'Administrador Nome', email: 'admin@email.com', cpf: '12345', password: '12345', tipo: 'Administrador')
