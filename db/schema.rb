3649f41541d7e565b7ebfb88b124e2161de3be19
victornun <vnfn@cin.ufpe.br>
2015-12-07 15:59:25 +0000

    Ajuste da migração db orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207031007) do
+ActiveRecord::Schema.define(version: 20151207023736) do
 
   create_table "machines", force: :cascade do |t|
     t.string   "tipping"
@@ -23,12 +23,12 @@ ActiveRecord::Schema.define(version: 20151207031007) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
+    t.string   "title"
     t.string   "local"
     t.text     "description"
     t.string   "status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "title"
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id"
